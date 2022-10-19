2ef758c421a49bb831a23aa338f663f06a0e4e8a
박수현 <soohyun.park@kt.com>
2022-10-19 15:40:28 +0900

    config-server 코드 추가'

--- userservice/src/main/java/com/example/userservice/controller/UserController.java
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -34,7 +34,12 @@ public class UserController {
 
 	@GetMapping("/health_check")
 	public String status() {
-		return "It's working in User Service on Port " + env.getProperty("local.server.port");
+		return "It's working in User Service on Port " + env.getProperty("local.server.port")
+			+ ", server.port=" + env.getProperty("server.port")
+			+ ", token.secret=" + env.getProperty("token.secret")
+			+ ", token.expiration-time=" + env.getProperty("token.expiration-time")
+			+ ", gateway.ip=" + env.getProperty("gateway.ip")
+			;
 	}
 
 	@GetMapping("/welcome")
